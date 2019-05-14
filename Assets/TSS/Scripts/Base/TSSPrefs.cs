061d3c42e7fd56397c43ac2e033ecba167251e34
Трубицын Владислав <obelardos@gmail.com>
2019-05-14 18:35:27 +0300

    1.6.21

--- Assets/TSS/Scripts/Base/TSSPrefs.cs
+++ Assets/TSS/Scripts/Base/TSSPrefs.cs
@@ -57,7 +57,7 @@ namespace TSS
     {
         #region Properties
 
-        public static string version { get { return @"1.6.2"; } }
+        public static string version { get { return @"1.6.21"; } }
 
         public static string author { get { return @"ObelardO"; } }
         public static string email { get { return @"obelardos@gmail.com"; } }
